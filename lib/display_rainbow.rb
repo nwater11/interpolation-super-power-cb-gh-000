415e1ef8cb9d478e00db2c469293f0d15f5e667e
Jane Smith <5904002440@rumail.ru.ac.th>
2017-09-13 18:11:28 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,14 @@
-# Write your #display_rainbow method here
+def display_rainbow(color)
+  red = ["R: red"]
+  orange = ["O: orange"]
+  yellow = ["Y: yellow"]
+  green = ["G: green"]
+  blue = ["B: blue"]
+  indigo = ["I: indigo"]
+  violet = ["V: violet"]
+  puts "#{color[0]}"
+
+end
+  
+  
+  
